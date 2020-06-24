b3aade9013e94dbb87ebbe790e739b1eaae70a00
Ariel Fuentes <ariel.fuentesdi@usach.cl>
2020-06-24 00:23:01 -0400

    Create main.R
    This will run all the reports.

---
+++ main.R
@@ -0,0 +1,16 @@
+#libraries
+library(rmarkdown)
+#calling base codes
+spp_gf <- magick::image_read("output/spp.gif")
+#run multiple Rmarkdown
+per <- unique(od_sersen$PER_DTPM2)
+rutas <- rts$ROUTE_NAME
+lapply(X = per, 
+       FUN = function (y) lapply(X = rutas, 
+                                 FUN = function(x) tryCatch(render(input = "code/Reporte2.Rmd", 
+                                                          output_file =  paste("reporte_", x, "_", y, ".html", sep=''), 
+                                                          output_dir = "output/", 
+                                                          encoding = "utf-8"),
+                                                          error = function(e) NULL)
+                                 )
+       )
