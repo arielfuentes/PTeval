fa80ee0b6df3a367a79f0dfc6b3213ceb451808f
Ariel <ariel.fuentesdi@usach.cl>
2021-10-14 10:27:26 -0300

    update image saving folder

--- code/main.R
+++ code/main.R
@@ -5,12 +5,13 @@ library(rmarkdown)
 source("code/wrg_dt.R", encoding = "utf-8")
 if(exists("output/spp.gif") == F){
   source("code/overlapDescrip.R", encoding = "utf-8")
+  file.copy(from = "spp.gif", to = "output/")
 }
 spp_gf <- magick::image_read("output/spp.gif")
 per <- unique(od_sersen$PER_DTPM2)
 per <- "04 - PMA"
 rutas <- rts$ROUTE_NAME
-rutas <- "108I"
+rutas <- "B02R"
 lapply(X = per, 
        FUN = function (y) lapply(X = rutas, 
                                  FUN = function(x) tryCatch(render(input = "code/Report.Rmd", 
